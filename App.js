3202aeb5e1bb97242a721eaf7b9053249c73e70a
Yannl <yann.legall@thalesgroup.com>
2023-06-15 20:07:19 +0200

    BT-011: Update header background color

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator
         screenOptions={{
-          headerTintColor: 'red',
+          headerTintColor: '#113f61',
           headerTransparent: true,
           headerStyle: { backgroundColor: 'transparent', color: 'transparent'}
         }}>
